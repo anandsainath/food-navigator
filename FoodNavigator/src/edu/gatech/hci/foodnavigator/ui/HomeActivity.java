f7e6e10abcddc603eb181f974120f2eebd10ed47
Jinhyun Kim <jinkim608@gmail.com>
2014-04-03 09:31:18 -0400

    Changed home screen to only enable "American" cuisine.

--- FoodNavigator/src/edu/gatech/hci/foodnavigator/ui/HomeActivity.java
+++ FoodNavigator/src/edu/gatech/hci/foodnavigator/ui/HomeActivity.java
@@ -7,7 +7,8 @@ import edu.gatech.hci.foodnavigator.BaseActivity;
 import edu.gatech.hci.foodnavigator.R;
 import edu.gatech.hci.foodnavigator.menu.AppMainMenu;
 
-public class HomeActivity extends BaseActivity implements AppHomeFragment.Callbacks {
+public class HomeActivity extends BaseActivity implements
+		AppHomeFragment.Callbacks {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -15,7 +16,8 @@ public class HomeActivity extends BaseActivity implements AppHomeFragment.Callba
 		setContentView(R.layout.app_home);
 		// Check if the layout rendered is for a small screen and
 		// add App Main Menu to the sliding menu frame.
-		if (findViewById(R.id.HomeView).getTag() == getResources().getString(R.string.small_screen)) {
+		if (findViewById(R.id.HomeView).getTag() == getResources().getString(
+				R.string.small_screen)) {
 			setMainMenu(new AppMainMenu());
 		}
 	}
@@ -24,15 +26,26 @@ public class HomeActivity extends BaseActivity implements AppHomeFragment.Callba
 	public void homeButtonClick(int resID) {
 		switch (resID) {
 		case R.id.home_american:
-			Toast.makeText(getApplicationContext(), R.string.usTitle, Toast.LENGTH_LONG).show();
+			Toast.makeText(getApplicationContext(), R.string.usTitle,
+					Toast.LENGTH_LONG).show();
+			startActivity(new Intent(this, FoodTrayActivity.class));
+
 			break;
 		case R.id.home_indian:
-			Toast.makeText(getApplicationContext(), R.string.inTitle, Toast.LENGTH_LONG).show();
+			Toast.makeText(getApplicationContext(),
+					"Will be available soon at only $1.99!", Toast.LENGTH_LONG)
+					.show();
+			// Toast.makeText(getApplicationContext(), R.string.inTitle,
+			// Toast.LENGTH_LONG).show();
 			break;
 		case R.id.home_korean:
-			Toast.makeText(getApplicationContext(), R.string.krTitle, Toast.LENGTH_LONG).show();
+			Toast.makeText(getApplicationContext(),
+					"Will be available soon at only $1.99!", Toast.LENGTH_LONG)
+					.show();
+			// Toast.makeText(getApplicationContext(), R.string.krTitle,
+			// Toast.LENGTH_LONG).show();
 			break;
 		}
-		startActivity(new Intent(this, FoodTrayActivity.class));
+
 	}
 }
