1cd2fc52fe8e95679320af6cfb44b992f38b2abe
Anand Sainath <anand.sainath@gmail.com>
2014-04-03 20:13:08 -0400

    Title appearance changed.

--- FoodNavigator/src/edu/gatech/hci/foodnavigator/ui/HomeActivity.java
+++ FoodNavigator/src/edu/gatech/hci/foodnavigator/ui/HomeActivity.java
@@ -6,18 +6,22 @@ import android.widget.Toast;
 import edu.gatech.hci.foodnavigator.BaseActivity;
 import edu.gatech.hci.foodnavigator.R;
 import edu.gatech.hci.foodnavigator.menu.AppMainMenu;
+import edu.gatech.hci.foodnavigator.widget.CustomTextView;
 
-public class HomeActivity extends BaseActivity implements
-		AppHomeFragment.Callbacks {
+public class HomeActivity extends BaseActivity implements AppHomeFragment.Callbacks {
+
+	CustomTextView TV_PageTitle;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.app_home);
+
+		TV_PageTitle = (CustomTextView) findViewById(R.id.TV_PageHeader);
+		TV_PageTitle.setText(R.string.home_page_title);
 		// Check if the layout rendered is for a small screen and
 		// add App Main Menu to the sliding menu frame.
-		if (findViewById(R.id.HomeView).getTag() == getResources().getString(
-				R.string.small_screen)) {
+		if (findViewById(R.id.HomeView).getTag() == getResources().getString(R.string.small_screen)) {
 			setMainMenu(new AppMainMenu());
 		}
 	}
@@ -26,22 +30,17 @@ public class HomeActivity extends BaseActivity implements
 	public void homeButtonClick(int resID) {
 		switch (resID) {
 		case R.id.home_american:
-			Toast.makeText(getApplicationContext(), R.string.usTitle,
-					Toast.LENGTH_LONG).show();
+			Toast.makeText(getApplicationContext(), R.string.usTitle, Toast.LENGTH_LONG).show();
 			startActivity(new Intent(this, FoodTrayActivity.class));
 
 			break;
 		case R.id.home_indian:
-			Toast.makeText(getApplicationContext(),
-					"Will be available soon at only $1.99!", Toast.LENGTH_SHORT)
-					.show();
+			Toast.makeText(getApplicationContext(), "Will be available soon at only $1.99!", Toast.LENGTH_SHORT).show();
 			// Toast.makeText(getApplicationContext(), R.string.inTitle,
 			// Toast.LENGTH_LONG).show();
 			break;
 		case R.id.home_korean:
-			Toast.makeText(getApplicationContext(),
-					"Will be available soon at only $1.99!", Toast.LENGTH_SHORT)
-					.show();
+			Toast.makeText(getApplicationContext(), "Will be available soon at only $1.99!", Toast.LENGTH_SHORT).show();
 			// Toast.makeText(getApplicationContext(), R.string.krTitle,
 			// Toast.LENGTH_LONG).show();
 			break;
